58f759010863562d67db922f2cf4b64977f18b7d
Frederick Johnson <fritzj96@gmail.com>
2020-04-26 13:05:54 -0700

    added: user entity

---
+++ src/user/User.entity.ts
@@ -0,0 +1,16 @@
+import { Column, PrimaryGeneratedColumn, Entity } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  public id: string;
+
+  @Column()
+  public name: string;
+
+  @Column({ unique: true })
+  public email: string;
+
+  @Column()
+  public password: string;
+}
