b40aedbb587f3b05dd383e18414841bff344ea7a
Frederick Johnson <fritzj96@gmail.com>
2020-04-26 13:10:49 -0700

    added: user controller progress

--- src/user/User.controller.ts
+++ src/user/User.controller.ts
@@ -1,9 +1,40 @@
-import { Router } from 'express';
-import { Controller } from '../interfaces';
+import { hash } from 'bcrypt';
 import { getRepository } from 'typeorm';
+import asyncHandler from 'express-async-handler';
+import { Router, Request, Response, NextFunction } from 'express';
+
+import { User } from '.';
+import { Controller } from '../interfaces';
+import {} from '../errors';
+import { CreateUserDto, LoginUserDto } from './dtos';
 
 export class UserController implements Controller {
   public path = '/user';
   public router = Router();
   private userRepo = getRepository(User);
+
+  constructor() {
+    this.initializeRoutes();
+  }
+
+  private initializeRoutes(): void {}
+
+  private register = asyncHandler(
+    async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+      const userData: CreateUserDto = req.body;
+      if (await this.userRepo.findOne({ email: userData.email })) {
+        next(new EmailTakenError(userData.email));
+      } else {
+        const hashedPassword = await hash(userData.password, 10);
+        const user = this.userRepo.create({
+          ...userData,
+          password: hashedPassword,
+        });
+        await this.userRepo.save(user);
+        user.password = undefined;
+        const token = this.createToken(user);
+        res.send({ user, token });
+      }
+    }
+  );
 }
