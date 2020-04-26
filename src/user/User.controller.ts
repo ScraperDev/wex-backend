47ca070a372a851b1b1349f6db90dc99cf29a47f
Frederick Johnson <fritzj96@gmail.com>
2020-04-26 13:05:06 -0700

    added: userRepo property

--- src/user/User.controller.ts
+++ src/user/User.controller.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 import { Controller } from '../interfaces';
+import { getRepository } from 'typeorm';
 
 export class UserController implements Controller {
   public path = '/user';
   public router = Router();
+  private userRepo = getRepository(User);
 }
