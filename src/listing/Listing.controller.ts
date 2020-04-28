ef45f622577d89404b160ab354d171fea5ed5c3f
Frederick Johnson <fritzj96@gmail.com>
2020-04-28 08:30:42 -0700

    added: getOneListing handler

--- src/listing/Listing.controller.ts
+++ src/listing/Listing.controller.ts
@@ -19,6 +19,7 @@ export class ListingController implements Controller {
 
   private initializeRoutes() {
     this.router.get(`${this.path}/`, authMiddleware, this.getActiveListings);
+    this.router.get(`${this.path}/:listingId`, authMiddleware, this.getOneListing);
     this.router.post(
       `${this.path}/`,
       authMiddleware,
@@ -30,6 +31,22 @@ export class ListingController implements Controller {
   private getActiveListings = asyncHandler(
     async (_req: RequestWithUser, res: Response, _next: NextFunction): Promise<void> => {
       res.send(await this.listingRepo.find({ where: { active: true } }));
+    }
+  );
+
+  private getOneListing = asyncHandler(
+    async (req: RequestWithUser, res: Response, next: NextFunction): Promise<void> => {
+      try {
+        const listing = await this.listingRepo.findOneOrFail(req.params.listingId);
+        if (!listing.active && req.user.id !== listing.owner.id) {
+          next(new ListingNotFoundError());
+          return;
+        }
+        listing.owner = undefined;
+        res.send(listing);
+      } catch {
+        next(new ListingNotFoundError());
+      }
     }
   );
 
